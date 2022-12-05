6008ac968c8d0cd36186d92e5a002fbcf151d8e6
confusezius <karsten.rh2@gmail.com>
2022-12-05 12:12:41 +0100

    Added main.py

--- example_runs.sh
+++ example_runs.sh
@@ -8,12 +8,12 @@ gpu="gpu_id"
 #### With simple LR scheduling - for relative comparisons, simply turn off scheduling.
 ### CUB-200
 # R128
-python dual_main.py --loss proxyvmf_panc --seed 0 --gpu $gpu --source_path $parent_folder --project $project --log_online --group Cub200_Prob_RN50_dim128 --dataset cub200 \
+python main.py --loss proxyvmf_panc --seed 0 --gpu $gpu --source_path $parent_folder --project $project --log_online --group Cub200_Prob_RN50_dim128 --dataset cub200 \
 --n_epochs 250 --tau 25 50 --gamma 0.3 --no_train_metrics --embed_dim 128 \
 --loss_proxyvmf_proxylrmulti 5000 --loss_proxyvmf_conclrmulti 20000 --loss_proxyvmf_concentration 6 --loss_proxyvmf_init_norm_multiplier 30 --loss_proxyvmf_temp 0.003 --loss_proxyvmf_templrmulti 200 --loss_proxyvmf_warmstart --loss_proxyvmf_n_samples 5 --loss_proxyvmf_guidance_w 3
 
 # R512
-python dual_main.py --loss proxyvmf_panc --seed 0 --gpu $gpu --source_path $parent_folder --project $project --log_online --group Cub200_Prob_RN50_dim512 --dataset cub200\ 
+python main.py --loss proxyvmf_panc --seed 0 --gpu $gpu --source_path $parent_folder --project $project --log_online --group Cub200_Prob_RN50_dim512 --dataset cub200\ 
 --n_epochs 250 --tau 31 --gamma 0.1 --no_train_metrics --embed_dim 512 \
 --loss_proxyvmf_proxylrmulti 5000 --loss_proxyvmf_conclrmulti 20000 --loss_proxyvmf_concentration 18 --loss_proxyvmf_init_norm_multiplier 50 --loss_proxyvmf_temp 0.003 --loss_proxyvmf_templrmulti 200 --loss_proxyvmf_warmstart --loss_proxyvmf_n_samples 5 --loss_proxyvmf_guidance_w 5
 
